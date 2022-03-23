16529170e85c844dbaa7625099aa6877ce25e317
yaozhichao <yaozhichao45@gmail.com>
2022-03-23 17:03:54 +0800

    适配BST部署模型的训练、测试、验证、导出onnx代码

--- models/common.py
+++ models/common.py
@@ -36,7 +36,7 @@ def autopad(k, p=None):  # kernel, padding
 
 class Conv(nn.Module):
     # Standard convolution
-    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=nn.ReLU()):  # ch_in, ch_out, kernel, stride, padding, groups
         super().__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
         self.bn = nn.BatchNorm2d(c2)
